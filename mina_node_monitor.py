c33b093c63cd311dce555d5d79e1d72bcb019b95
Krishna Bhaskaran <Krishna.Prasad@kindredgroup.com>
2021-05-22 21:03:06 +0100

    adding infinite loop and changes to Coda client

--- mina_node_monitor.py
+++ mina_node_monitor.py
@@ -4,7 +4,7 @@ import yaml
 import datetime
 import telegram
 import os
-from CodaClient import Client
+from MinaPyClient import Client
 from time import sleep
 
 c = yaml.load(open('config.yml', encoding='utf8'), Loader=yaml.SafeLoader)
@@ -14,29 +14,34 @@ CHAT_ID             = str(c["CHAT_ID"])
 NODE_NAME           = str(c["NODE_NAME"])
 GRAPHQL_HOST        = str(c["GRAPHQL_HOST"])
 GRAPHQL_PORT        = int(c["GRAPHQL_PORT"])
+ACCEPTABLE_BLOCK_DIFFERENCE        = int(c["ACCEPTABLE_BLOCK_DIFFERENCE"])
 
 bot=telegram.Bot(token=TELEGRAM_TOKEN)
 
-def get_node_status():
-    coda = Client(graphql_host=GRAPHQL_HOST, graphql_port=GRAPHQL_PORT)
-    daemon_status = coda.get_daemon_status()
-    sync_status = daemon_status['daemonStatus']['syncStatus']
-    uptime = daemon_status['daemonStatus']['uptimeSecs'] 	
-    blockchainLength = daemon_status['daemonStatus']['blockchainLength']
-    highestBlockLengthReceived = daemon_status['daemonStatus']['highestBlockLengthReceived']
-    highestUnvalidatedBlockLengthReceived = daemon_status['daemonStatus']['highestUnvalidatedBlockLengthReceived']
-    nextBlockTime = daemon_status['daemonStatus']['nextBlockProduction']['times'][0]['startTime']
-
-    return {    "sync_status": sync_status, 
-                "uptime": uptime, 
-                "blockchainLength": blockchainLength, 
-                "highestBlockLengthReceived": highestBlockLengthReceived, 
-                "nextBlockTime": nextBlockTime
-            }
 
 def send_message(chat_id, msg):
     bot.sendMessage(chat_id=chat_id, text=msg)
 
+def get_node_status():
+    try:
+        coda = Client(graphql_host=GRAPHQL_HOST, graphql_port=GRAPHQL_PORT)
+        daemon_status = coda.get_daemon_status()
+        sync_status = daemon_status['daemonStatus']['syncStatus']
+        uptime = daemon_status['daemonStatus']['uptimeSecs'] 	
+        blockchainLength = daemon_status['daemonStatus']['blockchainLength']
+        highestBlockLengthReceived = daemon_status['daemonStatus']['highestBlockLengthReceived']
+        highestUnvalidatedBlockLengthReceived = daemon_status['daemonStatus']['highestUnvalidatedBlockLengthReceived']
+        nextBlockTime = daemon_status['daemonStatus']['nextBlockProduction']['times'][0]['startTime']
+
+        return {    "sync_status": sync_status, 
+                    "uptime": uptime, 
+                    "blockchainLength": blockchainLength, 
+                    "highestBlockLengthReceived": highestBlockLengthReceived, 
+                    "nextBlockTime": nextBlockTime
+                }
+    except:
+        msg = "unable to reach mina daemon. Attention required!!!"
+        send_message(CHAT_ID, msg)
 
 def restart_node():
     #restart mina daemon
@@ -48,12 +53,38 @@ def restart_node():
     #update telegram on restart
     send_message(CHAT_ID, "mina daemon has been restarted")
 
-if __name__ == "__main__":
+def check_node_sync():
     d = get_node_status()
-    print(d["sync_status"])
-    print(d["uptime"])
-    print(d["blockchainLength"])
-    print(d["highestBlockLengthReceived"])
-    print(d["nextBlockTime"])
-    send_message(CHAT_ID, d["sync_status"])
-    restart_node()
+
+    if d["sync_status"] == "SYNCED":
+        block_height_difference = int(d["highestBlockLengthReceived"]) -  int(d["highestBlockLengthReceived"])
+
+        if block_height_difference > ACCEPTABLE_BLOCK_DIFFERENCE:
+            # routine for node not in sync starts
+            restart_node()
+            sleep(60*15)    
+        else:
+            msg = NODE_NAME + " in sync. Next block in " + str(d["nextBlockTime"])
+            send_message(CHAT_ID, msg)
+            sleep(60*15)
+
+        # print(d["sync_status"])
+        # print(d["uptime"])
+        # print(d["blockchainLength"])
+        # print(d["highestBlockLengthReceived"])
+        # print(d["nextBlockTime"])
+            
+    elif d["sync_status"] == "CATCHUP":
+        msg = NODE_NAME + " in catchup mode"
+        send_message(CHAT_ID, msg)
+        sleep(60*15)
+    
+    else:
+        msg = NODE_NAME + " in unknown mode. Attention required"
+        send_message(CHAT_ID, msg)
+        sleep(60*15)
+
+if __name__ == "__main__":    
+    while True:
+        check_node_sync()
+    
