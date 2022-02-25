982fa9b442335c61944bfed6cd03d84238ed2f26
7029732+shivlim@users.noreply.github.com <7029732+shivlim@users.noreply.github.com>
2022-02-25 22:23:08 +0000

    RPC monitor

--- rpc_node_monitor.py
+++ rpc_node_monitor.py
@@ -23,8 +23,9 @@ FANTOM_RPC_ENDPOINT = str(c["FANTOM_RPC_ENDPOINT"])
 ETH_RPC_ENDPOINT = str(c["ETH_RPC_ENDPOINT"])
 POLYGON_RPC_ENDPOINT = str(c["POLYGON_RPC_ENDPOINT"])
 MOONBEAM_RPC_ENDPOINT = str(c["MOONBEAM_RPC_ENDPOINT"])
+POLYGON_RPC_ENDPOINT2 = str(c["POLYGON_RPC_ENDPOINT2"])
 
-AVAX_RPC_REQUEST = {"jsonrpc": "2.0", "id": 1, "method": "info.isBootstrapped", "params": {"chain": "C"}}
+AVAX_RPC_REQUEST = {"jsonrpc": "2.0","method": "info.isBootstrapped","params":{"chain":"C"},"id":1}
 FANTOM_RPC_REQUEST = {"id": 1, "jsonrpc": "2.0", "method": "eth_syncing", "params": []}
 ETH_RPC_REQUEST = {"id": 1, "jsonrpc": "2.0", "method": "eth_syncing", "params": []}
 POLYGON_RPC_REQUEST = {"id": 1, "jsonrpc": "2.0", "method": "eth_syncing", "params": []}
@@ -35,7 +36,11 @@ COUNT = 0
 
 ln = logging.getLogger('main_logger')
 ln.setLevel('INFO')
+formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
+
 fh = logging.FileHandler('nodestatus_logs/nodestatus_{:%Y%m%d}.log'.format(datetime.datetime.now()))
+
+fh.setFormatter(formatter)
 ln.addHandler(fh)
 
 
@@ -63,57 +68,66 @@ if __name__ == "__main__":
     while True:
         try:
             formatted_text = ""
+            status_text = ""
 
             if formatted_text is None:
                 formatted_text = "Testnet - All Ok ✅ "
 
-            print(f'before avax {formatted_text}')
             responseavax, responseavaxstatus = getrpcendpointresponse(AVAX_RPC_ENDPOINT, AVAX_RPC_REQUEST)
-            print(f'after avax {formatted_text}')
             if responseavaxstatus and responseavax['result']['isBootstrapped']:
                 formatted_text += " avax_rpc_status ✅ "
+                status_text += "avaxstatus:true "
             else:
                 formatted_text += " avax_rpc_status ❌ "
+                status_text += "avaxstatus:false "
 
-            print(f'avx {formatted_text}')
 
 
             responsefantom, responsefantomstatus = getrpcendpointresponse(FANTOM_RPC_ENDPOINT, FANTOM_RPC_REQUEST)
             if responsefantomstatus and responsefantom['result'] is False:
                 formatted_text += " fantom_rpc_status ✅ "
+                status_text += "fantomstatus:true "
             else:
                 formatted_text += " fantom_rpc_status ❌ "
-
-            print(f'fantom {formatted_text}')
+                status_text += "fantomstatus:false "
 
 
             responseeth, responseethstatus = getrpcendpointresponse(ETH_RPC_ENDPOINT, ETH_RPC_REQUEST)
             if responseethstatus and responseeth['result'] is False:
                 formatted_text += " eth_rpc_status ✅ "
+                status_text += "ethstatus:true "
             else:
                 formatted_text += " eth_rpc_status ❌ "
-
-            print(f'eth {formatted_text}')
+                status_text += "ethstatus:false "
 
             responsemoonbeam, responsemoonbeamstatus = getrpcendpointresponse(MOONBEAM_RPC_ENDPOINT,
                                                                               MOONBEAM_RPC_REQUEST)
             if responsemoonbeamstatus and responsemoonbeam['result'] is False:
                 formatted_text += " moonbeam_rpc_status ✅ "
+                status_text += "monnbeamstatus:true "
             else:
                 formatted_text += " moonbeam_rpc_status ❌ "
-
-            print(f'moonbeam {formatted_text}')
-
+                status_text += "moonbeamstatus:false "
 
             responsepolygon, responsepolygonstatus = getrpcendpointresponse(POLYGON_RPC_ENDPOINT, POLYGON_RPC_REQUEST)
             if responsepolygonstatus and responsepolygon['result'] is False:
                 formatted_text += " polygon_rpc_status ✅ "
+                status_text += "polygonstatus:true "
             else:
                 formatted_text += " polygon_rpc_status ❌ "
+                status_text += "polygonstatus:false "
+
+            responsepolygon2, responsepolygonstatus2 = getrpcendpointresponse(POLYGON_RPC_ENDPOINT2, POLYGON_RPC_REQUEST)
+            if responsepolygonstatus2 and responsepolygon2['result'] is False:
+                formatted_text += " polygon_rpc_status2 ✅ "
+                status_text += "polygonstatus2:true "
+            else:
+                formatted_text += " polygon_rpc_status2 ❌ "
+                status_text += "polygonstatus2:false "
 
             print(f'final formatted_text {formatted_text}')
 
-            ln.info(formatted_text)
+            ln.info(status_text)
         except Exception as e:
             print(f'E {e}')
             msg = str(e)
